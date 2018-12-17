c15f481cd27fe3d6f1db2ca6d5ca6c42aa1bda7d
Danila Loginov <danila@loginov.rocks>
2018-12-17 04:46:53 +0300

    build release for GitHub Pages

---
+++ docs/precache-manifest.924821e04d6426e7f6e916e7003962b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f163eb4251143f9b7573",
+    "url": "./static/js/main.f163eb42.chunk.js"
+  },
+  {
+    "revision": "046c45db77d10c5bafa9",
+    "url": "./static/js/1.046c45db.chunk.js"
+  },
+  {
+    "revision": "046c45db77d10c5bafa9",
+    "url": "./static/css/1.b1228444.chunk.css"
+  },
+  {
+    "revision": "b5bc02e72247a18adf2ed085a2fd93ee",
+    "url": "./index.html"
+  }
+];
